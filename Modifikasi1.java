9d2ae16066bc82f21b2212a069a0f9af317328d9
Xyrfo <rofo.anggi18@gmail.com>
2024-10-11 10:23:46 +0700

    jawabpertanyaan1no5

---
+++ Modifikasi1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Modifikasi1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double nilai, tertinggi = 0, terendah = 100;
+        int jumlahLulus = 0,jumlahTidakLulus = 0;
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukan nilai mahasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            } 
+            if (nilai >= 60) {
+                jumlahLulus++;
+            } else {
+                jumlahTidakLulus++;           
+        } 
+    }
+        System.out.println("Nilai tertinggi: " + tertinggi);            
+        System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + jumlahLulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + jumlahTidakLulus);
+        }
+    }
+
